4588fec7db32a559c95fc643c7ed80bb9c503a82
info-mic-info <jobsocial94@gmail.com>
2022-12-05 16:24:01 +0100

    initial commit

---
+++ Palidroma/js/script.js
@@ -0,0 +1,28 @@
+
+
+
+
+let word = prompt(`Inserisci una parola`)
+
+let div_word= word.split("")
+console.log(div_word)
+
+let inverse_word = div_word.reverse()
+console.log (inverse_word)
+
+
+if (div_word===inverse_word){
+    console.log("palindrono")
+}
+
+else{
+    console.log(`Non è palindroma`)
+}
+
+ 
+// function palindromo {
+
+
+// }
+
+
